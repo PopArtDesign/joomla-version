06fefc256904700edbedd1d3bfcd2083a92fc27d
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2025-09-12 18:33:17 +0300

    feat: Add tests for Joomla version detection and output.

--- spec/joomla-version_spec.sh
+++ spec/joomla-version_spec.sh
@@ -14,17 +14,22 @@ Describe "joomla-version"
     The error should include "joomla-version: version file not found"
   End
 
-  Describe "Joomla 1.x"
-    It "prints the short version string"
-      When run ./joomla-version spec/fixtures/1.5.26
-      The status should be success
-      The output should eq "1.5.26"
+  Describe "detects version"
+    Parameters
+     1.5.26 "Joomla! 1.5.26 Stable [ senu takaa ama busani ] 27-March-2012 18:00 GMT"
+     2.5.28 "Joomla! 2.5.28 Stable [ Ember ] 10-December-2014 15:00 GMT"
     End
 
-    It "prints the long version string"
-      When run ./joomla-version --long spec/fixtures/1.5.26
+    It "prints the short version string for Joomla! ${1}"
+      When run ./joomla-version "spec/fixtures/${1}"
       The status should be success
-      The output should eq "Joomla! 1.5.26 Stable [ senu takaa ama busani ] 27-March-2012 18:00 GMT"
+      The output should eq "${1}"
+    End
+
+    It "prints the long version string for Joomla! ${1}"
+      When run ./joomla-version --long "spec/fixtures/${1}"
+      The status should be success
+      The output should eq "${2}"
     End
   End
 
